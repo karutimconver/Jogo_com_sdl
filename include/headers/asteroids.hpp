7f06dccfe536669af5a56028b2ba3cb0827e79b9
karutimconver <karutimconver@gmail.com>
2024-03-25 14:02:41 +0000

    working

--- include/headers/asteroids.hpp
+++ include/headers/asteroids.hpp
@@ -5,16 +5,22 @@
 #include <SDL/SDL_image.h>
 #include <headers/globals.hpp>
 
-class Asteroids {
+class Asteroid {
 private:
     unsigned short int r = 10;      // Raio padrão  
     unsigned short int angle = 90;  // Ângulo em graus
     float angular_speed = 2;        // Velocidade com que gira em graus por tick 
     unsigned short int size;        // Tamanho
 
+    void rotate();
+    void move();
 public:
     float x;
     float y;
     int radius;                     // raio do asteroide (caixa de colisão) raio = r * size
 
+    Asteroid();
+    ~Asteroid();
+    void update();
+    void draw(SDL_Renderer* renderer);
 };
