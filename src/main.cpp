fcc72925581c9b6915888d060ccbf7895275824a
karutimconver <karutimconver@gmail.com>
2024-03-25 14:20:01 +0000

    working

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,8 @@
 #include <headers/game.hpp>
-#include <windows.h>
+#include <ctime>
 
 int main(int argc, char* argv[]) {
+    srand(std::time(NULL));
     // Jogo
     Game game("Asteroids multiplayer", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGH, SDL_WINDOW_SHOWN);
 
