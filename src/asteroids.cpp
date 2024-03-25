c9f0898ba9a3c9ba9cd567c61d9b444ecca32390
karutimconver <karutimconver@gmail.com>
2024-03-25 21:19:51 +0000

    working

--- src/asteroids.cpp
+++ src/asteroids.cpp
@@ -37,15 +37,23 @@ void Asteroid::update() {
 };
 
 void Asteroid::draw(SDL_Renderer* renderer) {
+    // Criar array de pontos defenidos por coordenadas polares
     float points[this->vertices][2];
     for (int i = 0; i < this->vertices; i++) {
         float p_distance = radius + (((rand() % 20) * 0.1) - 1);
         float p_angle = angle + (((rand() % 20) * 0.1) - 1) + ((380 / vertices) * (i + 1));
         points[i][0] = p_distance;
         points[i][1] = p_angle;
-        std::cout << p_distance << " " << p_angle << "\n";
+    }
+
+    // Converter para arrays de coordenadas cartesianas
+    Sint16 array_x[this->vertices];
+    Sint16 array_y[this->vertices];
+
+    for (int j = 0; j < this->vertices; j++) {
+        array_x[j] = round(x + points[j][0] * cos(points[j][1] * M_PI / 180));
+        array_y[j] = round(y + points[j][0] * sin(points[j][1] * M_PI / 180));
+        std::cout << array_x[j] << " " << array_y[j] << "\n";
     }
     std::cout << "\n-----------\n";
-
-    
 };
